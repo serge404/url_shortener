f50e96fc3694b4c61e36bd97cceaedb8862f110a
Serge Selenou <selserge040404@gmail.com>
2025-03-21 18:47:13 -0400

    Adding database model for URLs

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ from flask_sqlalchemy import SQLAlchemy
 # Setting up Flask app
 app = Flask(__name__)
 
+# Setting up SQLAlchemy to use SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
+db = SQLAlchemy(app)
+
+# Define URL Model
+class URL(db.Model):
+    # Unique ID for each URL mapping
+    id = db.Column(db.Integer, primary_key=True)
+    # Each short URL should have a max of 12 chars
+    short_url = db.Column(db.String(12), unique=True, nullable=False)
+    # Maximum URL length supported by most browsers
+    original_url = db.Column(db.String(2048), nullable=False) 
+
+
 @app.route('/')
 def index():
     return "Index page for URL app"
 
 if __name__ == "__main__":
+    # Creating URL database table before any request is processed
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
