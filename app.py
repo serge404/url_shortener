98270fc276ac26a84ccdeb0186b0bdf11109fef5
Serge Selenou <selserge040404@gmail.com>
2025-03-21 19:22:48 -0400

    Creating short key function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import random, string
 
 # Setting up Flask app
 app = Flask(__name__)
@@ -12,11 +13,21 @@ db = SQLAlchemy(app)
 class URL(db.Model):
     # Unique ID for each URL mapping
     id = db.Column(db.Integer, primary_key=True)
-    # Each short URL should have a max of 12 chars
-    short_url = db.Column(db.String(12), unique=True, nullable=False)
+    # Each short URL should have a max of 8 chars
+    short_url = db.Column(db.String(8), unique=True, nullable=False)
     # Maximum URL length supported by most browsers
     original_url = db.Column(db.String(2048), nullable=False) 
 
+# Function to create the short key for the long URL 
+def create_short_url_key(length=8):
+    # The possible characters that can be used in the key
+    chars = string.ascii_letters + string.digits
+    
+    # Create the key by picking random chars until length is reached
+    key = ""
+    for i in range(length):
+        key += random.choice(chars)
+    return key
 
 @app.route('/')
 def index():
